3ac29bea06bbdfb2f7057a55ba6aecf59331e0d7
Garvit Goyal <garvitgoyal29557@gmail.com>
2026-06-04 22:05:46 +0530

    responsive

--- frontend/src/component/Contact.jsx
+++ frontend/src/component/Contact.jsx
@@ -30,7 +30,7 @@ function Contact() {
             })
             const res = await data.json()
             if (res.success) {
-                console.log(name," ",email," ",sub," ",mess)
+                console.log(name, " ", email, " ", sub, " ", mess)
                 alert("Message sent successfully!")
                 setname("")
                 setemail("")
@@ -52,7 +52,7 @@ function Contact() {
                 <Particles />
                 <div className='flex flex-col md:justify-center justify-evenly items-center w-full md:h-full h-screen md:pt-[5vh] pt-[1vh]'>
                     <h1 className='sora text-purple-400 text-center text-6xl md:text-4xl h-auto md:h-[10vh] mb-3 md:mb-0'>Contact</h1>
-                    <div className='w-full min-h-[80vh] md:h-[85vh] flex flex-col md:flex-row'>
+                    <div className='w-full min-h-[80vh] md:h-[85vh] flex flex-col md:flex-row items-center'>
                         <div className='w-full md:w-[50%] h-[34vh] md:h-full flex justify-center items-center'>
                             <motion.img
                                 animate={{
@@ -62,51 +62,46 @@ function Contact() {
                                     duration: 5,
                                     repeat: Infinity
                                 }}
-                                src={contactEle} alt="" className='scale-67 sm:scale-50 md:scale-70' />
+                                src={contactEle} alt="" className='scale-60 md:scale-80' />
                         </div>
-                        <div className='w-full md:w-[50%] h-full px-[3vw] md:px-[1vh] flex justify-center items-center'>
-                            <div className="w-full bg-purple-900/20 backdrop-blur-2xl rounded-2xl p-4 border border-white/20">
-                                <h1 className="text-3xl text-purple-400 mb-6 text-center sora">
-                                    Contact Me
-                                </h1>
-                                <form onSubmit={handlefrom}
-                                    className="flex flex-col gap-5">
-                                    <input
-                                        type="text"
-                                        placeholder="Your Name"
-                                        value={name}
-                                        onChange={(e) => setname(e.target.value)}
-                                        className="bg-transparent border border-white/20 rounded-lg px-1 py-2 text-white outline-none focus:border-purple-400 transition"
-                                    />
-                                    <input
-                                        type="email"
-                                        placeholder="Your Email"
-                                        value={email}
-                                        onChange={(e) => setemail(e.target.value)}
-                                        className="bg-transparent border border-white/20 rounded-lg px-1 py-2 text-white outline-none focus:border-purple-400 transition"
-                                    />
-                                    <input
-                                        type="text"
-                                        placeholder="Subject"
-                                        value={sub}
-                                        onChange={(e) => setsub(e.target.value)}
-                                        className="bg-transparent border border-white/20 rounded-lg px-1 py-2 text-white outline-none focus:border-purple-400 transition"
-                                    />
-                                    <textarea
-                                        rows="5"
-                                        placeholder="Your Message"
-                                        value={mess}
-                                        onChange={(e) => setmess(e.target.value)}
-                                        className="bg-transparent border border-white/20 rounded-lg px-1 py-2 text-white outline-none focus:border-purple-400 transition resize-none"
-                                    />
-                                    <button
-                                        type="submit"
-                                        className="mt-3 bg-purple-600 hover:bg-purple-700 transition text-white px-1 py-2 rounded-lg font-medium hover:scale-[1.02]"
-                                    >Send Message
-                                    </button>
-                                </form>
-                            </div>
+                        <div className="md:w-[50%] w-[90%] h-[80%] mx-[1vw] bg-purple-900/20 backdrop-blur-2xl rounded-2xl p-4 border border-white/20">
+                            <form onSubmit={handlefrom} className="flex flex-col md:h-full w-full h-full justify-around md:w-full">
+                                <input
+                                    type="text"
+                                    placeholder="Your Name"
+                                    value={name}
+                                    onChange={(e) => setname(e.target.value)}
+                                    className="bg-transparent border border-white/20 rounded-lg px-1 py-2 text-white outline-none focus:border-purple-400 transition"
+                                />
+                                <input
+                                    type="email"
+                                    placeholder="Your Email"
+                                    value={email}
+                                    onChange={(e) => setemail(e.target.value)}
+                                    className="bg-transparent border border-white/20 rounded-lg px-1 py-2 text-white outline-none focus:border-purple-400 transition"
+                                />
+                                <input
+                                    type="text"
+                                    placeholder="Subject"
+                                    value={sub}
+                                    onChange={(e) => setsub(e.target.value)}
+                                    className="bg-transparent border border-white/20 rounded-lg px-1 py-2 text-white outline-none focus:border-purple-400 transition"
+                                />
+                                <textarea
+                                    rows="5"
+                                    placeholder="Your Message"
+                                    value={mess}
+                                    onChange={(e) => setmess(e.target.value)}
+                                    className="bg-transparent border border-white/20 rounded-lg px-1 py-2 text-white outline-none focus:border-purple-400 transition resize-none"
+                                />
+                                <button
+                                    type="submit"
+                                    className="mt-0 md:mt-3 bg-purple-600 hover:bg-purple-700 transition text-white px-1 py-2 rounded-lg font-medium hover:scale-[1.02]"
+                                >Send Message
+                                </button>
+                            </form>
                         </div>
+
                     </div>
                 </div>
             </div>
